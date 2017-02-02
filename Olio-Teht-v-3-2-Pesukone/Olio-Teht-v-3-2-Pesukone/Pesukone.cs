0e2a479f8c1cccf91a4f9ae8ba9239dfb934dfea
h8324 <h8324@OHJE-330-WS-30>
2017-02-02 11:06:50 +0200

    initial

---
+++ Olio-Teht-v-3-2-Pesukone/Olio-Teht-v-3-2-Pesukone/Pesukone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_3_2_Pesukone
+{
+    class Pesukone
+    {
+    //ominaisuudet
+    public string malli { get; set; }
+    public int tilavuus { get; set; }
+    public int kierrosluku { get; set; }
+
+    }
+}
